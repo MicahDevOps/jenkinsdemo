1b3eb08873ab4b6955bb82e9961933984247b783
alicat <alicat@126.com>
2020-07-21 15:48:40 +0800

    change string

--- src/main/java/com/bolingcavalry/mavendockerplugindemo/controller/Hello.java
+++ src/main/java/com/bolingcavalry/mavendockerplugindemo/controller/Hello.java
@@ -11,6 +11,6 @@ public class Hello {
     @RequestMapping("/")
     public String sayHello(){
     	//修改返回的字符串的内容
-        return "jenkins ci test, " + new Date();
+        return "jenkins ci/cd test, " + new Date();
     }
 }
